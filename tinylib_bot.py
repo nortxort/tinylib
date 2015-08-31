208009dcab785a8e47e52843db32e4b99a68513e
nortxort <nortxort@users.noreply.github.com>
2015-08-31 22:27:27 +0200

    Update tinylib_bot.py

--- tinylib_bot.py
+++ tinylib_bot.py
@@ -32,7 +32,7 @@ def eightball():
     Magic eight ball.
     :return: a random answer str
     """
-    answers = ['It is certain', 'It is decidedly so', 'without a doubt', 'without a doubt', 'Yes definitely',
+    answers = ['It is certain', 'It is decidedly so', 'without a doubt', 'Yes definitely',
                'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
                'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now',
                'Concentrate and ask again', 'Don\'t count on it', 'My reply is no', 'My sources say no',
